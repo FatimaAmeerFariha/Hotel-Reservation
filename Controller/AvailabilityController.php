28c882730958ab65505390415450b0e416027ed0
FatimaAmeerFariha <fatimaameerfariha@gmail.com>
2025-06-28 22:53:56 +0600

    Update AvailabilityController.php

--- Controller/AvailabilityController.php
+++ Controller/AvailabilityController.php
@@ -2,7 +2,6 @@
 require_once '../includes/db.php';
 require_once '../Model/AvailabilityModel.php';
 
-// 🟢 Handle billing info (POST)
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $room = floatval($_POST['room_charge'] ?? 0);
     $service = floatval($_POST['service_tax'] ?? 0);
@@ -16,7 +15,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     exit();
 }
 
-// 🔵 Handle availability booking (GET)
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['name'], $_GET['room_type'], $_GET['checkin'], $_GET['checkout'], $_GET['guest_no'])) {
     $guest_name = $_GET['name'];
     $room_type = $_GET['room_type'];
@@ -26,7 +24,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['name'], $_GET['room_typ
 
     AvailabilityModel::bookRoom($guest_name, $room_type, $checkin, $checkout, $guest_no);
 
-    // Redirect to billing summary with all same GET data
     $query = http_build_query($_GET);
     header("Location: ../View/BillingSummary.php?$query");
     exit();
